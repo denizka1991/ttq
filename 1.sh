8adcd62f75ccd4b68868803d3e433a4deb91d91f
dkhaliul <hdenizka@gmail.com>
2020-03-05 12:59:58 +0200

    qweqwe

--- 1.sh
+++ 1.sh
@@ -1,4 +1,4 @@
 git add --all
 git commit -m "qweqwe"
 git push origin
-ssh dkhal@10.111.1.24 -p 50683 'su cd /var/www/html && su git pull'
+ssh dkhal@10.111.1.24 -p 50683 'sudo cd /var/www/html && sudo git pull'
